89a728ec10f155cc68ca5297f9907d2747969eef
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-10-19 21:13:23 -0500

    built basic structure for hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,1 +1,18 @@
 import "./hero.scss";
+
+export default function Hero() {
+  return (
+    <>
+      <div>Socials</div>
+      <div>
+        <h1>Hi, I'm X'Zaiver</h1>;<p>Fullstack Developer</p>
+        <p>
+          I'm a web developer based in the USA. I enjoy building fullstack
+          applications with React and Rails.
+        </p>
+        <button>About Me</button>
+      </div>
+      <div>Image</div>
+    </>
+  );
+}
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,3 +1,5 @@
+import Hero from "../hero/Hero.tsx";
+
 export default function Home() {
-  return <h1>Hi, I'm X'Zaiver</h1>;
+  return <Hero />;
 }
