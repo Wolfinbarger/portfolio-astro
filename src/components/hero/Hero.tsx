2dfd6cf50d48e68430799409f2dd6c80d2b85726
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-10-19 21:35:10 -0500

    added social images

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,16 +1,51 @@
 import "./hero.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Hero() {
   return (
     <>
-      <div>Socials</div>
       <div>
-        <h1>Hi, I'm X'Zaiver</h1>;<p>Fullstack Developer</p>
+        <ul>
+          <li>
+            <a href="">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                style={{ color: "#7ed321" }}
+                className="envelope"
+                href="xzaiver.wolfinbarger@gmail.com"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ color: "#7ed321" }}
+                className="linkedin"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Wolfinbarger">
+              <FontAwesomeIcon
+                icon={faSquareGithub}
+                style={{ color: "#7ed321" }}
+                className="github"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>Hi, I'm X'Zaiver</h1>
+        <p>Fullstack Developer</p>
         <p>
           I'm a web developer based in the USA. I enjoy building fullstack
           applications with React and Rails.
         </p>
-        <button>About Me</button>
+        <a href="#/about">About Me</a>
       </div>
       <div>Image</div>
     </>
