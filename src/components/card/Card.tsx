1236840a454f4de4451e190ce121623c47d2c73b
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-10-21 23:37:14 -0500

    position external links to bottom and style as button

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -14,9 +14,13 @@ export default function Card(work) {
         <p>{work.value.description}</p>
         <div className="tags">{tags}</div>
         <div className="links">
-          <a href={work.value.url}>Website</a> <br />
-          <a href="{work.value.source}">
-            <i className="devicon-github-original source"></i>
+          <a href={work.value.url} className="link">
+            <i className="bx bx-link-external"></i>
+            Demo
+          </a>{" "}
+          <br />
+          <a href="{work.value.source}" className="link">
+            <i className="devicon-github-original"></i>
             Source
           </a>
         </div>
