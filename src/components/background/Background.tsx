606444815914f146f4615027711e00f47ef73d13
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-11-16 21:06:40 -0600

    started creating toggle tab for experience section

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -1,3 +1,12 @@
 import "./background.scss";
 
-export default function Background() {}
+import {useState} from "react";
+import BackgroundItems from "./BackgroundItem";
+import { Education, Experience } from "./Data";
+
+export default function Background() {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {setToggleState(index)};
+  return ()
+}
