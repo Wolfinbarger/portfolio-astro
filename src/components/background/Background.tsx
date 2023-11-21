18df2dd19a1e42debddcbaa50252f795d10ecc7a
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-11-20 20:07:46 -0600

    Add accessiblity in scss for body. font-size 16px, line-height 1.5, letter-spacing noarmal

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -7,6 +7,6 @@ import { Education, Experience } from "./Data";
 export default function Background() {
   const [toggleState, setToggleState] = useState(1);
 
-  const toggleTab = (index) => {setToggleState(index)};
+  const toggleTab = (index: number) => {setToggleState(index)};
   return ()
 }
